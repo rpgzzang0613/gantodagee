65eeaef801fbfc55e57e34328c3d8167f47c48de
rpgzzang0613 <rpgzzang@naver.com>
2017-04-02 00:03:18 +0900

    모두의 파이썬 책 완료

---
+++ myPy/19A-set.py
@@ -0,0 +1,22 @@
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print(A)
+print(B)
+
+print(1 in A)
+print(6 in A)
+print(len(A))
+
+print(A | B)
+print(A & B)
+print(A - B)
+
+C = {x for x in range(1, 11)}
+D = {x for x in range(1, 11) if x % 3 == 0}
+
+print(C)
+print(D)
+
+print(C < D)
+print(C > D)
